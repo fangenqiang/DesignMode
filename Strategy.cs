b5ea396e5207285a44a031e854ec4269060c1680
fangenqiang <765881336@qq.com>
2019-07-04 11:12:37 +0800

    策略模式

---
+++ Strategy.cs
@@ -0,0 +1,22 @@
+﻿//==================================================
+// Copyright (C) 2016-2019 Fanjia
+// All rights reserved
+// CLR 版本: 4.0.30319.42000
+// 机器名称: PC-20180807FPRA
+// 文 件 名: Strategy
+// 创建时间: 2019/7/4 10:23:10
+// 创 建 人: 范根强
+//==================================================
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignMode
+{
+    /// <summary>
+    /// 策略模式
+    /// </summary>
+    public class Strategy
+    {
+    }
+}
