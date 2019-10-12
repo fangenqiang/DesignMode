3a045441af4b807f68dda1d4b52640bb22da364a
fangenqiang <765881336@qq.com>
2019-10-12 17:03:02 +0800

    中介者模式
    中介者模式

---
+++ Mediator.cs
@@ -0,0 +1,102 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DesignMode
+{
+    /// <summary>
+    /// 中介者模式
+    /// </summary>
+    public class MediatorPattern
+    {
+        /// <summary>
+        /// 抽象中介类
+        /// </summary>
+        public abstract class Mediator
+        {
+            public abstract void Common(string type);
+        }
+        /// <summary>
+        /// 抽象房东类
+        /// </summary>
+        public abstract class Houser
+        {
+            public abstract void Handle();
+        }
+        /// <summary>
+        /// 房东1
+        /// </summary>
+        public class SmallHouser : Houser
+        {
+            public override void Handle()
+            {
+                Console.WriteLine("单间——便宜整洁");
+            }
+        }
+        /// <summary>
+        /// 房东2
+        /// </summary>
+        public class TwoHouser : Houser
+        {
+            public override void Handle()
+            {
+                Console.WriteLine("两居室——合适，靠谱");
+            }
+        }
+        /// <summary>
+        /// 房东3
+        /// </summary>
+        public class ThreeHouser : Houser
+        {
+            public override void Handle()
+            {
+                Console.WriteLine("三居室——大气，宽松");
+            }
+        }
+        /// <summary>
+        /// 房屋中介
+        /// </summary>
+        public class HouseMediator : Mediator
+        {
+            private SmallHouser smallHouser;
+            private TwoHouser twoHouser;
+            private ThreeHouser threeHouser;
+
+            public void SetSmallHouse(SmallHouser small)
+            {
+                smallHouser = small;
+            }
+
+            public void SetTwoHouse(TwoHouser two)
+            {
+                twoHouser = two;
+            }
+            public void SetThreeHouse(ThreeHouser three)
+            {
+                threeHouser = three;
+            }
+            public override void Common(string type)
+            {
+
+                switch (type)
+                {
+                    case "单间":
+                        smallHouser.Handle();
+                        Console.WriteLine("如果可以就可以租房了!");
+                        break;
+                    case "两居室":
+                        twoHouser.Handle();
+                        Console.WriteLine("如果可以就可以租房了!");
+                        break;
+                    case "三居室":
+                        threeHouser.Handle();
+                        Console.WriteLine("如果可以就可以租房了!");
+                        break;
+                    default:
+                        Console.WriteLine($"{type}暂时没有房源!");
+                        break;
+                }
+            }
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,5 +1,5 @@
 ﻿using System;
-
+using static DesignMode.MediatorPattern;
 
 namespace DesignMode
 {
@@ -14,9 +14,33 @@ namespace DesignMode
             //Singleton.Singleton5.GetSingleton();
             //Singleton.Singleton6.GetSingleton();
 
-            ChannelService.SetChannel(ServiceType.AliPay).Pay();
-           
+            //ChannelService.SetChannel(ServiceType.AliPay).Pay();
+            #region 中介者模式
+
+            Console.WriteLine("一个租客看房：");
+            ///初始化中介
+            HouseMediator mediator = new HouseMediator();
+
+            ///初始化房屋信息
+            SmallHouser smallHouser = new SmallHouser();
+            TwoHouser twoHouser = new TwoHouser();
+            ThreeHouser threeHouse = new ThreeHouser();
+
+            ///中介获取房屋信息
+            mediator.SetSmallHouse(smallHouser);
+            mediator.SetTwoHouse(twoHouser);
+            mediator.SetThreeHouse(threeHouse);
+
+            ///租户A需要两居室、提供看房
+            mediator.Common("两居室");
+
+            //租户B需要四居室、暂无房源
+            mediator.Common("四居室");
+
+            #endregion
             Console.WriteLine("Hello World!");
+
+            Console.ReadKey();
         }
     }
 }
